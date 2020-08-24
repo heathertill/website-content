030c318e819ae65e6d042128115c4dc4f568f286
Heather Till <heather.till@charter.net>
2020-08-24 12:34:34 -0500

    -created queries and routes for landing and about info

--- src/server/routes/api/landingInfo.ts
+++ src/server/routes/api/landingInfo.ts
@@ -3,6 +3,38 @@ import queries from '../../db';
 
 const router = Router();
 
+router.get('/:id', async (req, res, next) => {
+    let id = req.params.id;
+    try {
+        let [landing] = await queries.LandingInfo.getLandingInfo(id);
+        res.json(landing);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
+router.post('/', async (req, res, next) => {
+    let body = req.body;
+    try {
+        let newLandingInfo = await queries.LandingInfo.newLandingInfo(body);
+        res.json(newLandingInfo);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.put('/id:', async (req, res, next) => {
+    let id = req.params.id;
+    let body = req.body;
+    try {
+        let editLandingInfo = await queries.LandingInfo.editLandingInfo(body, id);
+        res.json(editLandingInfo);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+    })
 
 export default router;
