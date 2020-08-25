d24f527a799fa16426de86cc7df247709ab20522
Heather Till <heather.till@charter.net>
2020-08-25 13:04:49 -0500

    -setting up form action for landing info

--- src/client/components/public/LandingInfo.tsx
+++ src/client/components/public/LandingInfo.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
+import { User, json } from '../../utils/api';
+import SubmitEdit from '../../utils/submitEdit';
 
 export interface LandingInfoProps { }
 
@@ -9,7 +11,52 @@ const LandingInfo: React.SFC<LandingInfoProps> = () => {
     const [branding, setBranding] = useState('');
     const [callToAction, setCallToAction] = useState('');
     const [simWebFunc, setSimWebFunc] = useState('');
+    const [editable, setEditable] = useState(false);
 
+    const canEdit = async () => {
+        if (User.userid) {
+            try {
+                let landing = await json(`/api/landingInfo/${User.userid}`)
+                if (landing !== null) {
+                    setEditable(true),
+                        setSiteEntry(landing.siteEntry),
+                        setBranding(landing.branding),
+                        setCallToAction(landing.callToAction),
+                        setSimWebFunc(landing.simWebFunc)
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
+
+    useEffect(() => { canEdit() }, [])
+
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        let body = {
+            userid: User.userid,
+            siteEntry,
+            branding,
+            callToAction,
+            simWebFunc
+        }
+        if (editable === false) {
+            try {
+                let newLandingInfo = await('/')
+            } catch (e) {
+                console.log(e)
+            }
+
+        } else {
+            try {
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    }
 
 
     return (
@@ -27,19 +74,15 @@ const LandingInfo: React.SFC<LandingInfoProps> = () => {
                 </div>
                 <div>
                     <label htmlFor="" > </label>
-                    <input className="form-control" type="text" value={} placeholder={}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => set(e.target.value)} />
+                    <input className="form-control" type="text" value={callToAction} placeholder={callToAction}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCallToAction(e.target.value)} />
                 </div>
                 <div>
                     <label htmlFor="" > </label>
-                    <input className="form-control" type="text" value={} placeholder={}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => set(e.target.value)} />
+                    <input className="form-control" type="text" value={simWebFunc} placeholder={simWebFunc}
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSimWebFunc(e.target.value)} />
                 </div>
-                <div>
-                    <label htmlFor="" > </label>
-                    <input className="form-control" type="text" value={} placeholder={}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => set(e.target.value)} />
-                </div>
+                <SubmitEdit editable />
             </form>
         </section>
     );
